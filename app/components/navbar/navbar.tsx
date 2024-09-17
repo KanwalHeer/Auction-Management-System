84000e1cca3a6a256bcd79aba62f1a87454fc33a
ali langah <alilangah78@gmail.com>
2024-09-18 03:58:42 +0500

    ...

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,40 +1,46 @@
-'use client'
-import { useState } from 'react';
-import Link from 'next/link';
-import { MdMenu, MdClose } from 'react-icons/md';
+'use client';
+import Link from "next/link";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export default function Navbar() {
+ 
+  // Set toggle state
+  const [toggleMenu, setToggleMenu] = useState(false);
+  // Toggle function
+  const toggleMenuHandle = () => {
+    setToggleMenu(!toggleMenu);
+  };
 
   return (
-    <nav className="bg-green-600 p-2">
-      <div className="container mx-auto flex justify-center items-center">
-       
-        <button
-          className="text-white md:hidden"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? <MdClose size={24} /> : <MdMenu size={24} />}
-        </button>
-        <div
-          className={`${
-            isOpen ? 'block' : 'hidden'
-          } md:flex md:items-center md:space-x-4`}
-        >
-          <Link href="/" className="text-white hover:text-green-100">
-            Home
-          </Link>
-         
-          <Link href="/auctionList" className="text-white hover:text-green-100">
-            Auctions
-          </Link>
-          <Link href="/createAuction" className="text-white hover:text-green-100">
-            Create Action
-          </Link>
+    <div>
+      {/* Navbar for md and lg screens */}
+      <div className="hidden md:flex lg:flex items-center justify-center py-6 text-blue-700 font-bold px-6">
+        {/* Links on the left side */}
+        <div className="flex space-x-8">
+          <Link href={'/'} className="text-lg hover:border-b-2 hover:border-blue-800">Home</Link>
+          <Link href={'/auctionList'} className="text-lg hover:border-b-2 hover:border-blue-800">Auctions</Link>
+          <Link href={'/createAuction'} className="text-lg hover:border-b-2 hover:border-blue-800">Creat Auction</Link>
         </div>
       </div>
-    </nav>
-  );
-};
+      {/* Navbar for small screens */}
+      <div className="flex md:hidden justify-center items-end p-3 text-blue-700 font-bold rounded-md">
+      <div>
+        <button onClick={toggleMenuHandle} className="text-2xl">
+          {toggleMenu ? <FaTimes /> : <FaBars />}
+        </button>
+        </div>
+      </div>
 
-export default Navbar;
+      {/* Menu for small screens */}
+      {toggleMenu && (
+        <div className="lg:hidden text-white font-bold bg-blue-700/30 p-4">
+          <Link href={'/'} className="block px-4 py-2 hover:bg-blue-700 text-lg">Home</Link>
+          <Link href={'/auctionList'} className="block px-4 py-2 hover:bg-blue-700 text-lg">Auctions</Link>
+          <Link href={'/createAuction'} className="block px-4 py-2 hover:bg-blue-700 text-lg">Creat Auction</Link>
+          
+        </div>
+      )}
+    </div>
+  );
+}
